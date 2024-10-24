59bb24bb1d4ad3c231d5a358346745dd128862e8
bmoprogrammer <bmoprogrammer@gmail.com>
2024-10-24 16:38:08 -0500

    Button functionality and changing sprite upon answering. Also adjusting neon square orange border.

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
+    int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
     void Start()
     {
         questionText.text = question.GetQuestion();
@@ -19,4 +23,20 @@ public class Quiz : MonoBehaviour
         }
     }
 
+    public void OnAnswerSelected(int index)
+    {
+        Image buttonImage;
+        if(index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!";
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            questionText.text = "Sorry, the correct answer is:\n" + question.GetAnswers(question.GetCorrectAnswerIndex());
+            buttonImage = answerButtons[question.GetCorrectAnswerIndex()].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+    }
 }
